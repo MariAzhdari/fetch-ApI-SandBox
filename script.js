af3ff3ece2fc7dedc975f4b461844cd4af6e4123
Mari Azh <Marzi.aghchay@gmail.com>
2023-02-06 13:42:29 +0000

    text

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const btn = document.getElementById('btn');
+const text = document.getElementById('text');
 btn.addEventListener('click',getText);
 
 function  getText(){
-fetch('sample.txt').then(res => res.text()).then(data => console.log(data));
+fetch('sample.txt')
+.then(res => res.text())
+.then(data => {text.innerHTML = data })
+.catch(er => console.log(er))
 }
